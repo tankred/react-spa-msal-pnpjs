9e2188355fb8a6ff395dcf043d762cdb66028511
Kurt Duyck <kurt.duyck@vives.be>
2019-09-11 15:26:47 +0200

    Lab msal - pulpo
    	modified:   app/src/msal/msalConfig.ts
    	modified:   webpack/webpack.dev.js

--- app/src/msal/msalConfig.ts
+++ app/src/msal/msalConfig.ts
@@ -1,4 +1,4 @@
 export const msalConfig = {
-  authority: 'https://adfs.vives.be/adfs/oauth2/authorize/',
+  authority: 'https://adfs.vives.be/adfs/',
   clientId: '4b91979e-7cf4-4d3a-8de5-1abba4f7c705'
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -8,7 +8,7 @@ const commonPaths = require('./common-paths');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 56668;
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || 'localhost';
 
